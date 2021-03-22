6b64e5c53fcfd834c127d32aa425e9ede8de91fa
Alperen Sancak <49764317+ByAlperenS@users.noreply.github.com>
2021-03-22 23:06:26 +0300

    Update index.php

--- Rasni/index.php
+++ Rasni/index.php
@@ -5,7 +5,7 @@ include __DIR__ . '/Rasni.php';
 
 use Discord\Discord;
 
-$discord = new Discord(['token' => 'ODE2Mjk2NTA4MTY4MDc3MzUz.YD45XQ.QS_1fh1-R_yOUXptZtniGvZyqkE']);
+$discord = new Discord(['token' => '~~~']);
 $main = new Rasni($discord);
 $main->onCommands();
 $main->onEvents();
